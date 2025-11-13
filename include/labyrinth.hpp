3016bed4ee757908da965429e9b37dfba9d6ff61
Александр Ёлочка <elochkaan@gmail.com>
2025-11-13 13:33:08 +0700

    fix: change coordinates types to Vector2u

--- include/labyrinth.hpp
+++ include/labyrinth.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "generator.hpp"
 #include "player.hpp"
+#include <SFML/System/Vector2.hpp>
 #include <cstddef>
-#include <utility>
 #include <vector>
 
 struct Cell
@@ -31,11 +31,11 @@ public:
   
 private:
   void update();
-  void setEnd(std::pair<size_t, size_t> end);
+  void setEnd(sf::Vector2u end);
   std::vector<std::vector<Cell>> map_;
   std::vector<std::vector<char>> reprOfmap_;
   Generator* generator_ = nullptr;
   Player* player_ = nullptr;
   static Labyrinth* instance_;
-  std::pair<size_t, size_t> end_;
+  sf::Vector2u end_;
 };
