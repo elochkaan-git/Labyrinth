b9b8757e18c645326877d4f9dc92fd13b92121d6
Александр Ёлочка <elochkaan@gmail.com>
2025-11-13 13:32:47 +0700

    feat!: add shape, change move, change pos_ type to Vector2u

--- include/player.hpp
+++ include/player.hpp
@@ -1,21 +1,23 @@
 #pragma once
-#include <cstddef>
-#include <utility>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/System/Vector2.hpp>
 
 class Labyrinth;
 
 class Player
 {
 public:
-  Player(Labyrinth* owner);
+  Player(Labyrinth* owner, float radius);
   ~Player() = default;
   static Player* getInstance();
-  static void init(Labyrinth* owner);
-  void move(size_t x, size_t y);
-  std::pair<size_t, size_t> getPos() const;
+  static void init(Labyrinth* owner, float radius);
+  const sf::CircleShape& getShape() const; 
+  sf::Vector2u getPos() const;
+  void move(char direction);
 
 private:
-  std::pair<size_t, size_t> pos_ = {1, 1};
+  sf::Vector2u pos_ = {1, 1};
   static Player* instance_;
   Labyrinth* owner_;
+  sf::CircleShape shape_;
 };
--- src/player.cpp
+++ src/player.cpp
@@ -1,12 +1,16 @@
 #include "player.hpp"
 #include "labyrinth.hpp"
-#include <cstddef>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/System/Vector2.hpp>
 
 Player* Player::instance_ = nullptr;
 
-Player::Player(Labyrinth* owner)
+Player::Player(Labyrinth* owner, float radius)
+  : shape_(radius)
 {
   owner_ = owner;
+  shape_.setPosition(
+    { radius / 0.49f * getPos().x, radius / 0.49f * getPos().y });
 }
 
 Player*
@@ -16,21 +20,40 @@ Player::getInstance()
 }
 
 void
-Player::init(Labyrinth* owner)
+Player::init(Labyrinth* owner, float radius)
 {
-  instance_ = new Player(owner);
+  instance_ = new Player(owner, radius);
 }
 
 void
-Player::move(size_t x, size_t y)
+Player::move(char direction)
 {
   auto& map = owner_->getReprOfMap();
-  if(map[y][x] != '#')
-    pos_ = {x, y};
+  sf::Vector2u pos = getPos();
+  if (direction == 'r')
+    pos.x++;
+  if (direction == 'l')
+    pos.x--;
+  if (direction == 'd')
+    pos.y++;
+  if (direction == 'u')
+    pos.y--;
+
+  if (map[pos.y][pos.x] != '#') {
+    pos_ = pos;
+    shape_.setPosition(
+      { shape_.getRadius() / 0.49f * pos.x, shape_.getRadius() / 0.49f * pos.y });
+  }
 }
 
-std::pair<size_t, size_t>
+sf::Vector2u
 Player::getPos() const
 {
   return pos_;
+}
+
+const sf::CircleShape&
+Player::getShape() const
+{
+  return shape_;
 }
